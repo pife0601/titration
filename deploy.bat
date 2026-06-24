a7b9b1f1cc03dc8dc83310c84324a08d6f7e8cfa
pife0601 <pife0601@users.noreply.github.com>
2026-06-24 12:11:20 +0900

    Update

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,10 @@
 @echo off
+set PATH=%PATH%;C:\Program Files\Git\cmd;C:\Program Files\GitHub CLI
 "C:\Program Files\Git\cmd\git.exe" config --global user.name "pife0601"
 "C:\Program Files\Git\cmd\git.exe" config --global user.email "pife0601@users.noreply.github.com"
-"C:\Program Files\Git\cmd\git.exe" init
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Update"
 "C:\Program Files\Git\cmd\git.exe" branch -M main
-"C:\Program Files\GitHub CLI\gh.exe" repo create titration --public --source=. --remote=origin --push
+"C:\Program Files\GitHub CLI\gh.exe" repo create titration --public --source=. --remote=origin
+"C:\Program Files\Git\cmd\git.exe" push -u origin main
 cmd /c npm run deploy
